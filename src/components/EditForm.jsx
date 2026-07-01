25f788257af8fa250f3a86c09ff948dc1644da71
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2026-07-01 14:21:54 +0000

    feat: save inputs in state

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -4,21 +4,30 @@ import '../styles/EditForm.css';
 export default function EditForm({ sectionTitle, fieldList }) {    
     const [fields, setFields] = useState({});
 
-    //function handleSubmit(e) {
-    //    setFields[{...fields, fields.: e.target.value}]
-    //}
+    function handleSubmit(e) {
+        e.preventDefault();
+        const id = 'form-' + formatter(sectionTitle);
+        const form = document.getElementById(id);
+        const formData = new FormData(form);
+        const updatedFields = {};
+        for (const [key, value] of formData) {
+            console.log(key + ": " + value);
+            updatedFields[key] = value;
+        }
+        setFields(updatedFields);
+    }
     
     return(
         <>
-            <h3>{sectionTitle}</h3>
-            <form className={formatter(sectionTitle)}>
+        <h3>{sectionTitle}</h3>
+            <form id={'form-' + formatter(sectionTitle)} onSubmit={handleSubmit}>
                 <div className="fields">
                     {fieldList.map(field => {
                         const formattedField = formatter(field);
                         return(
                             <div className={formattedField} key={formattedField}>
-                                <label>{field + ':'}</label>
-                                <input></input>
+                                <label htmlFor={formattedField}>{field + ':'}</label>
+                                <input id={formattedField} name={formattedField}></input>
                             </div>
                         )
                     })}
